96e628600557cd7471d20814a6c68b662a08bf83
Kamil <you@example.com>
2024-11-09 19:42:41 +0100

    modify header and refactor

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+/**
+ * Adds a 'visible' class to each '.artist-section' element when it is scrolled into view.
+ * The 'visible' class is added when the top of the element is within 100 pixels of the viewport's bottom.
+ */
 document.addEventListener("scroll", function() {
     document.querySelectorAll(".artist-section").forEach((section) => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top < window.innerHeight - 100) {
+        if (section.getBoundingClientRect().top < window.innerHeight - 100) {
             section.classList.add("visible");
         }
     });
